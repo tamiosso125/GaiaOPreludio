be09afc71093d3ace280ab10c4359a7bca4d107e
Guilherme Ribeiro Lemos Tamiosso <guilherme.tamiosso@hotmail.com>
2023-04-18 11:15:53 -0500

    feat: Footer structure

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,87 @@
-import { ReactNode } from 'react';
-
+import React from "react";
 
 export function Footer() {
-  return (
-    <>
-      <h1>Footer</h1>
-    </>
-  );
+	return (
+		<>
+			<div className="bg-gray-50 h-1/2 w-full flex md:flex-row flex-col justify-around items-start p-20">
+				<div className="p-5 ">
+					<ul>
+						<p className="text-gray-800 font-bold text-3xl pb-6">
+							Gaia O Preludio
+						</p>
+						<div className="flex gap-6 pb-5">
+							img
+						</div>
+					</ul>
+				</div>
+				<div className="p-5">
+					<ul>
+						<p className="text-gray-800 font-bold text-2xl pb-4">text</p>
+						<li className="text-gray-500 text-md pb-2 font-semibold hover:text-orange-600 cursor-pointer">
+            text
+						</li>
+						<li className="text-gray-500 text-md pb-2 font-semibold hover:text-orange-600 cursor-pointer">
+            text
+						</li>
+						<li className="text-gray-500 text-md pb-2 font-semibold hover:text-orange-600 cursor-pointer">
+            text
+						</li>
+						<li className="text-gray-500 text-md pb-2 font-semibold hover:text-orange-600 cursor-pointer">
+            text
+						</li>
+					</ul>
+				</div>
+				<div className="p-5">
+					<ul>
+						<p className="text-gray-800 font-bold text-2xl pb-4">text</p>
+						<li className="text-gray-500 text-md pb-2 font-semibold hover:text-orange-600 cursor-pointer">
+            text
+						</li>
+						<li className="text-gray-500 text-md pb-2 font-semibold hover:text-orange-600 cursor-pointer">
+            text
+						</li>
+						<li className="text-gray-500 text-md pb-2 font-semibold hover:text-orange-600 cursor-pointer">
+            text
+						</li>
+						<li className="text-gray-500 text-md pb-2 font-semibold hover:text-orange-600 cursor-pointer">
+            text
+						</li>
+						<li className="text-gray-500 text-md pb-2 font-semibold hover:text-orange-600 cursor-pointer">
+            text
+						</li>
+					</ul>
+				</div>
+				<div className="p-5">
+					<ul>
+						<p className="text-gray-800 font-bold text-2xl pb-4">text</p>
+						<li className="text-gray-500 text-md pb-2 font-semibold hover:text-orange-600 cursor-pointer">
+            text
+						</li>
+						<li className="text-gray-500 text-md pb-2 font-semibold hover:text-orange-600 cursor-pointer">
+            text
+						</li>
+						<li className="text-gray-500 text-md pb-2 font-semibold hover:text-orange-600 cursor-pointer">
+            text
+						</li>
+						<li className="text-gray-500 text-md pb-2 font-semibold hover:text-orange-600 cursor-pointer">
+            text
+						</li>
+						<li className="text-gray-500 text-md pb-2 font-semibold hover:text-orange-600 cursor-pointer">
+            text
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div className="flex flex-col justify-center items-center text-center  p-5 bg-gray-50">
+				<h1 className=" text-gray-800 font-semibold">
+        text
+					<span className="hover:text-orange-600 font-semibold cursor-pointer">
+					text
+					</span>
+				</h1>
+			</div>
+		</>
+	);
 }
+
+export default Footer;
